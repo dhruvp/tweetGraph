936efe0ac515a7ffbd3e78748960aa9805b4c722
Dhruv Parthasarathy <dhruvp@mit.edu>
2013-06-14 11:39:23 -0700

    production compile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,9 @@ TweetGraph::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
+  # config.assets.js_compressor = NoCompression.new
+  # config.assets.css_compressor = NoCompression.new
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
